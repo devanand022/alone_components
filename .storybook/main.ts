febaefefeced843d3984b1912c1f490cd8e34324
devanand022 <ananddeva578@gmail.com>
2025-04-10 14:49:49 +0530

    fixing build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,18 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
-import { fileURLToPath } from 'url';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)'],
-
   addons: [
     '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
-
   typescript: {
     check: false,
     checkOptions: {},
@@ -26,37 +24,25 @@ const config: StorybookConfig = {
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
   },
-
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
-
   webpackFinal: async (config) => {
     config.module?.rules?.push(
       {
         test: /\.module\.(css|scss)$/,
         exclude: [path.resolve(__dirname, 'node_modules')],
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /(\.css|\.scss)$/,
         exclude: [path.resolve(__dirname, 'node_modules')],
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     );
-
     return config;
   },
-
   docs: {},
 };
 
